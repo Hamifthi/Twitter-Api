5d42b058085404512a5439b3ecb288d8b3d89a7c
Hamifthi <hamedonil@yahoo.com>
2018-03-12 19:07:18 +0330

    complete everything i need for twitter analyze

--- TwitterApiHandler.py
+++ TwitterApiHandler.py
@@ -28,14 +28,34 @@ class Twitter:
         self.api = tweepy.API(self.auth)
         self.listner = Listner()
 
+    def rateLimitStatus(self):
+        return self.api.rate_limit_status()
+        DataBase
+
     def serachTimeLine(self, id, count):
         if count > 200:
             raise ValueError('max count is 200')
-        print(self.api.user_timeline(id, count = count))
+        data = self.api.user_timeline(id, count = count)
+        userTimeLine = DataBase.TimeLine(data = **data, name = id)
+        userTimeLine.save()
 
-    def searchTweets (self, object):
-        dataBase.save(self.api.search(str(object) if type(object) != list else object, lang = 'en'))
+    def searchTweets (self, object, lang, result_type = 'mixed', count = 200):
+        if count > 200:
+            raise ValueError('max count is 200')
+        self.api.search(str(object) if type(object) != list else object, lang = lang, result_type = result_type)
+        DataBase
     
-    def stream (self, object, lang):
+    def stream (self, object):
         streamer = tweepy.Stream(self.auth, self.listner)
-        streamer.filter(track = str(object) if type(object) != list else object, language = lang)
+        streamer.filter(track = str(object) if type(object) != list else object)
+        streamObject = DataBase.StreamObjects(data = dataDict, subject = object)
+        streamObject.save()
+
+    def cursor (self, method, **kwargs):
+        items = tweepy.Cursor(method, kwargs).items()
+        pages = tweepy.Cursor(method, kwargs).pages()
+        return items, pages
+
+    def api (self, method, **kwargs):
+        return self.api.method(**kwargs)
+        DataBase
